635d1678640e673fc3dfa994bffae8d6aa7e7b11
Gourav Singh Rawat <grvrawat999@gmail.com>
2021-11-24 19:26:45 +0530

    implemented date and time
    made functions on date and time

--- js/main.js
+++ js/main.js
@@ -7,15 +7,16 @@ let weatherCon = document.getElementById("weather-con");
 const tempStatus = "Clouds";
 const getCurrentDate = () => {
     const weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+    weekday[0] = "Sun";
+    weekday[1] = "Mon";
+    weekday[2] = "Tue";
+    weekday[3] = "Wed";
+    weekday[4] = "Thu";
+    weekday[5] = "Fri";
+    weekday[6] = "Sat";
     let currentTime = new Date();
-    console.log(weekday[currentTime.getDay()]);
+    let day = weekday[currentTime.getDay()];
+    return day;
 }
 
 const getCurrentTime = () => {
@@ -23,10 +24,12 @@ const getCurrentTime = () => {
         "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
     ]
     var now = new Date();
-    var month = months[now.getMonth() + 1];
+    var month = months[now.getMonth()];
     var date = now.getDay();
     let hours = now.getHours();
     let minutes = now.getMinutes();
+    return `${month} ${date} | ${hours}:${minutes}`;
 }
-// curDate.innerHTML = getCurrentTime() + "|"
+
+curDate.innerHTML = getCurrentDate() + " | " + getCurrentTime();
 // Wed | Oct 10 | 10:00
